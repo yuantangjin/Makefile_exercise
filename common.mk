9c46324ee2badc86ca49960f1c3c31382cee9b33
yuantangjin <1739773664@qq.com>
2022-02-08 09:32:47 +0800

    support for static-lib generation and usage

--- common.mk
+++ common.mk
@@ -9,26 +9,39 @@ LINK_OBJ_DIR = $(BUILD_ROOT)/app/link_obj
 $(shell mkdir -p $(LINK_OBJ_DIR))
 DEP_DIR = $(BUILD_ROOT)/app/dep
 $(shell mkdir -p $(DEP_DIR))
+LIB_OBJ_DIR = $(BUILD_ROOT)/app/lib_obj
+$(shell mkdir -p $(LIB_OBJ_DIR))
+LIB_DIR = $(BUILD_ROOT)/lib
+
+OBJ_DIR = $(LINK_OBJ_DIR)
+ifneq ("$(LIB)","")
+OBJ_DIR = $(LIB_OBJ_DIR)
+endif
+
+OBJS := $(addprefix $(OBJ_DIR)/,$(OBJS))
+DEPS := $(addprefix $(DEP_DIR)/,$(DEPS))
+LIB  := $(addprefix $(LIB_DIR)/,$(LIB))
 
 LINK_OBJ = $(wildcard $(LINK_OBJ_DIR)/*.o)
-
-OBJS := $(addprefix $(LINK_OBJ_DIR)/,$(OBJS))
-DEPS := $(addprefix $(DEP_DIR)/,$(DEPS))
-
 LINK_OBJ += $(OBJS)
 
+LIB_DEP = $(wildcard $(LIB_DIR)/*.a)
+LINK_LIB_NAME = $(patsubst lib%,-l%,$(basename $(notdir $(LIB_DEP))))
+
 #BIN = 
-all:$(DEPS) $(OBJS) $(BIN)
+all:$(DEPS) $(OBJS) $(LIB) $(BIN)
 #	@echo "OBJS=$(OBJS)"
 ifneq ("$(wildcard $(DEPS))","")
 include $(DEPS)
 endif
-$(BIN):$(LINK_OBJ)
-	gcc -o $@ $^ 
-$(LINK_OBJ_DIR)/%.o:%.c
+$(BIN):$(LINK_OBJ) $(LIB_DEP)
+	gcc -o $@ $^ -L$(LIB_DIR) $(LINK_LIB_NAME)
+$(LIB):$(OBJS)
+	ar rcs $@ $^
+$(OBJ_DIR)/%.o:%.c
 	gcc -I$(HEAD_PATH) -o $@ -c $(filter %.c,$^)
 $(DEP_DIR)/%.d:%.c
-	gcc -I$(HEAD_PATH) -MM $(filter %.c,$^) | sed 's,\(.*\)\.o[ :]*,$(LINK_OBJ_DIR)/\1.o $@:,g '> $@
+	gcc -I$(HEAD_PATH) -MM $(filter %.c,$^) | sed 's,\(.*\)\.o[ :]*,$(OBJ_DIR)/\1.o $@:,g '> $@
 clean:
 	@echo "OBJS = $(OBJS)"
 	rm -f $(BIN) $(OBJS) $(DEPS)
--- makefile
+++ makefile
@@ -3,6 +3,7 @@ export BUILD_ROOT = $(shell pwd)
 export HEAD_PATH = $(BUILD_ROOT)/inc
 
 all:
+	$(shell mkdir -p lib)
 	make -C lcd
 	make -C media
 	make -C usb
@@ -11,3 +12,5 @@ clean:
 	rm -rf mp3
 	rm -rf app/link_obj
 	rm -rf app/dep
+	rm -rf app/lib_obj
+	rm -rf lib
